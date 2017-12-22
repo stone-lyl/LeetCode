9606426f7f38333c2cfe694bc4be829a5b0793ca
stone <stone-lyl@outlook.com>
2017-12-22 09:55:39 +0800

    10，11完成

---
+++ 10/isMatch-10.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+    for(let i = 0; i<s.length; i++){
+        s[i]
+    }
+    console.log(s === p);
+};
+isMatch("aa", "aa");
