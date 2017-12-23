3e0454b95705d82b8966caf58e7883385df9fef8
stone <stone-lyl@outlook.com>
2017-12-23 23:11:36 +0800

    121股票问题

--- 1-array/BestStock-121.js
+++ 1-array/BestStock-121.js
@@ -1,13 +1,29 @@
 /**
  * 只允许买一股
+ * 
  * Input: [7, 1, 5, 3, 6, 4]
 Output: 5
-
 max. difference = 6-1 = 5 
 (not 7-1 = 6, as selling price needs to be larger than buying price)
- * @param {number[]} prices
+ *
+ *思考本题最重要有两个因素： 1. 买入和抛差距最大的时候 gap  2. 时间：买入 < 抛出 
+ gap 记录符合情况下的所有差距， 通过max比较。
+ min始终为当前最小值
+ *  @param {number[]} prices
  * @return {number}
  */
 var maxProfit = function (prices) {
-
+    let min = prices[0], max = 0;
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] - min < 0) {
+            min = prices[i];
+        } else {
+            max = max > prices[i] - min ? max : prices[i] - min;
+        }
+    }
+    if (prices.length <= 1) {
+        return 0;
+    }
+    return max;
 };
+console.log(maxProfit([1,2]));
--- test.js
+++ test.js
@@ -10,39 +10,30 @@
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function (head, n) {
-    if (n === 0) return head;
+navWithRouter = () => withRouter(({ history }) => (
+    <Nav
+        groups={[{ links: this.props.routes.filter((r: any) => !r.hide).map(ParseGroup) }]}
+        onLinkClick={
+            (ev?: React.MouseEvent<HTMLElement>, item?: INavLink) => {
+                if (item && item.key) {
+                    history.push(item.key);
+                }
+            }}
+    />
 
-    const shouldUpdateRemoveNode = count => count < 0;
+))
 
-    const paddedHead = {
-        next: head
-    };
-
-    let removeNode = paddedHead;
-    let nextNode = paddedHead;
-    let i = n; // 2
-
-    while (nextNode) {
-        if (shouldUpdateRemoveNode(i)) removeNode = removeNode.next;
-        nextNode = nextNode.next;
-        i--;
-    }
-
-    removeNode.next = removeNode.next.next;
-
-    return paddedHead.next;
-
-};
-
-function ListNode(val) {
-    this.val = val;
-    this.next = null;
+withRouter = ({history}) => {
+    <Nav
+        groups={[{ links: this.props.routes.filter((r: any) => !r.hide).map(ParseGroup) }]}
+        onLinkClick={
+            (ev?: React.MouseEvent<HTMLElement>, item?: INavLink) => {
+                if (item && item.key) {
+                    history.push(item.key);
+                }
+            }}
+    />
 }
-
-var list1 = new ListNode(1);
-list1.next = new ListNode(2);
-list1.next.next = new ListNode(3);
-list1.next.next.next = new ListNode(4);
-list1.next.next.next.next = new ListNode(5);
-console.log(removeNthFromEnd(list1, 3));
+navWithRouter = (withRouter) => {
+    withRouter({history});
+}
