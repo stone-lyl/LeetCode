126e45df89bb617c43442dac071cc0521a788805
stone <stone-lyl@outlook.com>
2020-06-14 21:41:51 +0800

    feat: flat, event-loop

--- zhufeng/event-loop.js
+++ zhufeng/event-loop.js
@@ -1,19 +1,21 @@
 async function async1() {
     console.log('1 start');
-    await async2();
+    const a = await async2();
+    console.log(a);
     console.log('1 end');
 }
 
 async function async2() {
-    console.log(2)
+    console.log(2);
+    return '这才是回调？';
 }
-console.log('start');
 
 setTimeout(() => {
     console.log('set time out');
 }, 0);
 
 async1();
+console.log('start');
 
 new Promise(function (resolve) {
     console.log('promise 1');
@@ -22,5 +24,7 @@ new Promise(function (resolve) {
     console.log('promise 2');
 })
 console.log('end');
+
+// start -- 1 start -- promise 1 -- end -- 2 -- 1 end --- promise2  --- set time out  
 
 // start - 1 start - 2 - promise 1 - end - 1 end - promise 2 - set time out (浏览器答案正确)
